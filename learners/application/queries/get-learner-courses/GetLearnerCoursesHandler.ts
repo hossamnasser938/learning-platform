c4ec2c57562722487e5dacb76e77dae344f5c7ba
Hossam Abdelnaser <hossam.abdelnaser@myoncare.com>
2025-08-31 18:09:48 +0300

    handle learner not found when fetching learner courses

--- learners/application/queries/get-learner-courses/GetLearnerCoursesHandler.ts
+++ learners/application/queries/get-learner-courses/GetLearnerCoursesHandler.ts
@@ -1,19 +1,26 @@
 import { GetLearnerCoursesQuery } from "./GetLearnerCoursesQuery";
-import { ICourseEnrollmentRepo } from "@l-p/learners/domain/contracts";
+import { ICourseEnrollmentRepo, ILearnerRepo } from "@l-p/learners/domain/contracts";
 import { inject, injectable } from "@l-p/shared/infrastructure/dependency-injection/utils";
-import { courseEnrollmentRepoID, coursesGatewayID } from "@l-p/learners/infrastructure/dependency-injection/tokens";
+import { courseEnrollmentRepoID, coursesGatewayID, learnerRepoID } from "@l-p/learners/infrastructure/dependency-injection/tokens";
 import { IGetLearnerCoursesHandler } from "./IGetLearnerCoursesHandler";
 import { ICoursesGateway } from "@l-p/learners/infrastructure/gateway";
 import { GetCoursesResponse } from "@l-p/courses/api/responses";
+import { LearnerNotFoundException } from "@l-p/learners/domain/models/learner/exceptions";
 
 @injectable()
 export class GetLearnerCoursesHandler implements IGetLearnerCoursesHandler {
   constructor(
+    @inject(learnerRepoID) private readonly learnerRepo: ILearnerRepo,
     @inject(courseEnrollmentRepoID) private readonly courseEnrollmentRepo: ICourseEnrollmentRepo,
     @inject(coursesGatewayID) private readonly coursesGateway: ICoursesGateway
   ) {}
 
   async handle(query: GetLearnerCoursesQuery): Promise<GetCoursesResponse> {
+    const learner = await this.learnerRepo.getById(query.learnerId);
+    if (!learner) {
+      throw new LearnerNotFoundException(query.learnerId);
+    }
+
     const courseIds = await this.courseEnrollmentRepo.getLearnerEnrollments(query.learnerId);
     
     return await this.coursesGateway.getCoursesByIds(courseIds);
