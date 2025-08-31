4a556104378c4d9c8e50143f68c5c017c0e760f8
Hossam Abdelnaser <hossam.abdelnaser@myoncare.com>
2025-08-31 18:05:42 +0300

    handle chapter not found when fetching chapter lessons

--- courses/application/queries/get-chapter-lessons/GetChapterLessonsHandler.ts
+++ courses/application/queries/get-chapter-lessons/GetChapterLessonsHandler.ts
@@ -1,18 +1,25 @@
 import { GetChapterLessonsQuery } from "./GetChapterLessonsQuery";
-import { ILessonRepo } from "@l-p/courses/domain/contracts";
+import { IChapterRepo, ILessonRepo } from "@l-p/courses/domain/contracts";
 import { inject, injectable } from "@l-p/shared/infrastructure/dependency-injection/utils";
-import { lessonRepoID } from "@l-p/courses/infrastructure/dependency-injection/tokens";
+import { chapterRepoID, lessonRepoID } from "@l-p/courses/infrastructure/dependency-injection/tokens";
 import { Lesson } from "@l-p/courses/domain/models";
 import { IGetChapterLessonsHandler } from "./IGetChapterLessonsHandler";
+import { ChapterNotFoundException } from "@l-p/courses/domain/models/chapter/exceptions/ChapterExceptions";
 
 @injectable()
 export class GetChapterLessonsHandler implements IGetChapterLessonsHandler
 {
   constructor(
+    @inject(chapterRepoID) private readonly chapterRepo: IChapterRepo,
     @inject(lessonRepoID) private readonly lessonRepo: ILessonRepo
   ) {}
 
   async handle(query: GetChapterLessonsQuery): Promise<Lesson[]> {
+    const chapter = await this.chapterRepo.getById(query.chapterId);
+    if (!chapter) {
+      throw new ChapterNotFoundException(query.chapterId);
+    }
+
     return await this.lessonRepo.getByChapterId(query.chapterId);
   }
 }
